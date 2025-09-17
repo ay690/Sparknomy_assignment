44af9d6854800fbd6742b9d75fb5429d258e1037
Aniket Yadav <aniketyadav690@gmail.com>
2025-09-17 19:51:17 +0530

    UI: card component

---
+++ src/components/common/Card/Card.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { cn } from '../../../lib/utils';
+
+interface CardProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export const Card: React.FC<CardProps> = ({ children, className }) => {
+  return (
+    <div className={cn('bg-white rounded-lg border border-gray-200 shadow-sm', className)}>
+      {children}
+    </div>
+  );
+};
+
+interface CardContentProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export const CardContent: React.FC<CardContentProps> = ({ children, className }) => {
+  return (
+    <div className={cn('p-4', className)}>
+      {children}
+    </div>
+  );
+};
